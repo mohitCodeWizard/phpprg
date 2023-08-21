2b6adbd218f57f5344844a3128f5df94af3ac141
mohitgithubpatel <mohitnaliydara63@gmail.com>
2023-08-21 09:51:40 +0530

    done

--- a2p3disp.php
+++ a2p3disp.php
@@ -10,7 +10,7 @@
                 }
             }
 
-            xmlhttp.open("GET", "a2p3disp.php?q=" + str, true);
+            xmlhttp.open("GET", "a2p3search.php?q=" + str, true);
             xmlhttp.send();
 
 
@@ -20,14 +20,36 @@
 
 <body>
     <?php
-    if(isset($_GET["txtsearch"])!= ""){
-        echo "<div id='disp_area'></div>";
-    }
-    else{
-        echo "<input type='text' size = '30' name = 'txtsearch' onkeyup='showresult(this.value)'><br><br>";
-        echo "<div id='disp_area'></div>";
-    }
 
+    echo "<input type='text' size = '30' name = 'txtsearch' onkeyup='showresult(this.value)'><br><br>";
+    echo "<div id='disp_area'></div>";
+    $conn = mysqli_connect("localhost", "root", "", "student");
+    $response ="";
+    if (isset($_GET['col'])) {
+        $response .= "<table border = '2'>";
+        $sql = "show columns from student  ";
+        $res = mysqli_query($conn, $sql);
+        $response .= "<tr>";
+        while ($row = mysqli_fetch_array($res)) {
+            $response .= "<td><a href='a2p3disp.php?col=$row[Field]'>$row[Field]</a></td>";
+        }
+        $response .= "</tr>";
+
+        $sql = "select * from student order by " . $_GET['col'];
+        $res = mysqli_query($conn, $sql);
+        while ($row = mysqli_fetch_array($res)) {
+            $response .= "<tr>";
+            for ($i = 0; $i < 7; $i++) {
+                $response .= "<td>$row[$i]</td>";
+            }
+            $response .= "<td><a href='a2p3update.php?semail=$row[1]'>Update</a></td>";
+            $response .= "<td><a href='a2p3delete.php?semail=$row[1]'>Delete</a></td>";
+
+            $response .= "</tr>";
+        }
+        $response .= "</table>";
+        echo $response;
+    }
     ?>
 
 </body>
@@ -35,46 +57,3 @@
 </html>
 
 <?php
-
-$conn = mysqli_connect("localhost", "root", "", "student");
-$response = "";
-if(isset($_GET['q'])){
-    $response .= "<table border = '2'>";
-    $sql = 'show columns from student';
-    $res = mysqli_query($conn, $sql);
-    
-    $response .= "<tr>";
-    while ($row = mysqli_fetch_array($res)) {
-        $response .= "<td><a href='a2p3disp.php?col=$row[Field]'>$row[Field]</a></td>";
-    }
-    $response .= "</tr>";
-    $str =$_GET['q'];
-    
-    if ($str != "") {
-        $sql = "select * from student where
-            name like '%$str%' or email like '%$str%' or pass like '%$str%' or
-            phone like '%$str%' or gender like '%$str%'";
-    } 
-    else {
-        $sql = "select * from student";
-    }
-    
-    if (isset($_GET['col'])) {
-        $sql = $sql . " order by " . $_GET['col'];
-    }
-    $res = mysqli_query($conn, $sql);
-    while ($row = mysqli_fetch_array($res)) {
-        $response .= "<tr>";
-        for ($i = 0; $i < 7; $i++) {
-            $response .= "<td>$row[$i]</td>";
-        }
-        $response .= "<td><a href='a2p3update.php?semail=$row[1]'>Update</a></td>";
-        $response .= "<td><a href='a2p3delete.php?semail=$row[1]'>Delete</a></td>";
-    
-        $response .= "</tr>";
-    }
-    $response .= "</table>";
-    echo $response;
-}
-
-?>
--- a2p3search.php
+++ a2p3search.php
@@ -1,38 +1,68 @@
 <?php
-    // $conn = mysqli_connect("localhost","root","","student");
-    // $response = "";
-    // $response .= "<table border = '2'>";
-    // $sql = 'show columns from student';
-    // $res = mysqli_query($conn, $sql);
+$conn = mysqli_connect("localhost", "root", "", "student");
+$response = "";
+$str ="";
+if(isset($_GET['q'])){
+    $response .= "<table border = '2'>";
+    $sql = 'show columns from student';
+    $res = mysqli_query($conn, $sql);
     
-    // $response .= "<tr>";
-    // while ($row = mysqli_fetch_array($res)) {
-    //     $response.= "<td><a href='a2p3disp.php?col=$row[Field]'>$row[Field]</a></td>";
-    // }
-    // $response .= "</tr>";
-    // $str = $_GET['q'];
-    // if($str != ""){
-    //     $sql = "select * from student where
-    //     name like '%$str%' or email like '%$str%' or pass like '%$str%' or
-    //     phone like '%$str%' or gender like '%$str%'";
-    // }else{
-    //     $sql = "select * from student";
-    // }
+    $response .= "<tr>";
+    while ($row = mysqli_fetch_array($res)) {
+        $response .= "<td><a href='a2p3disp.php?col=$row[Field]'>$row[Field]</a></td>";
+    }
+    $response .= "</tr>";
+    $str =$_GET['q'];
     
+    if ($str != "") {
+        $sql = "select * from student where
+            name like '%$str%' or email like '%$str%' or pass like '%$str%' or
+            phone like '%$str%' or gender like '%$str%'";
+    } 
     // if (isset($_GET['col'])) {
     //     $sql = $sql . " order by " . $_GET['col'];
     // }
-    // $res = mysqli_query($conn,$sql);
-    // while($row = mysqli_fetch_array($res)){
-    //     $response .="<tr>";
-    //     for($i=0;$i<7;$i++){
-    //         $response.= "<td>$row[$i]</td>";
-    //     }
-    //     $response .= "<td><a href='a2p3update.php?semail=$row[1]'>Update</a></td>";
-    //     $response .= "<td><a href='a2p3delete.php?semail=$row[1]'>Delete</a></td>";
+    $res = mysqli_query($conn, $sql);
+    while ($row = mysqli_fetch_array($res)) {
+        $response .= "<tr>";
+        for ($i = 0; $i < 7; $i++) {
+            $response .= "<td>$row[$i]</td>";
+        }
+        $response .= "<td><a href='a2p3update.php?semail=$row[1]'>Update</a></td>";
+        $response .= "<td><a href='a2p3delete.php?semail=$row[1]'>Delete</a></td>";
     
-    //     $response.="</tr>";
+        $response .= "</tr>";
+    }
+    $response .= "</table>";
+    echo $response;
+}
+else{
+    $response .= "<table border = '2'>";
+    $sql = 'show columns from student';
+    $res = mysqli_query($conn, $sql);
+    
+    $response .= "<tr>";
+    while ($row = mysqli_fetch_array($res)) {
+        $response .= "<td><a href='a2p3disp.php?col=$row[Field]'>$row[Field]</a></td>";
+    }
+    $response .= "</tr>";
+    $sql = "select * from student";
+    // if (isset($_GET['col'])) {
+    //     $sql = $sql . " order by " . $_GET['col'];
     // }
-    // $response .="</table>";
-    // echo $response;
+    $res = mysqli_query($conn, $sql);
+    while ($row = mysqli_fetch_array($res)) {
+        $response .= "<tr>";
+        for ($i = 0; $i < 7; $i++) {
+            $response .= "<td>$row[$i]</td>";
+        }
+        $response .= "<td><a href='a2p3update.php?semail=$row[1]'>Update</a></td>";
+        $response .= "<td><a href='a2p3delete.php?semail=$row[1]'>Delete</a></td>";
+    
+        $response .= "</tr>";
+    }
+    $response .= "</table>";
+    echo $response;
+}
+
 ?>
